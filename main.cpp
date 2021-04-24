c4f2a9e9f19aa07d41f3c87b24ea4f07bf0c9ce9
SabaGvaramadze <79360799+SabaGvaramadze@users.noreply.github.com>
2021-04-24 03:38:42 -0700

    cart fix

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <conio.h>
 #include <iostream>
+#include <map>
+//#include <pair>
 #include "DataBase/ItemSearch.h"
 #include "DataBase/Inventory.h"
 #include "DataBase/cartItemStruct.h"
@@ -7,6 +9,13 @@
 int main (){
 	int sumPrice=0;
 	std::vector<DB::cartItem> cart;
+	std::map<std::string,int> pendingList;
+	
+	std::vector<DB::item> list = DB::loadList();
+	for(auto Item = list.begin();Item!=list.end();Item++){
+		pendingList.insert(std::pair<std::string,char>((*Item).name,0));
+	}
+	
 	while(true){
 	std::vector<DB::item> list = DB::loadList();
 	std::cout << "commands: ---- :cart / ItemName  / :purchase ---------\n";
@@ -28,20 +37,23 @@ int main (){
 		system("cls");
 		for(auto cartItem = cart.begin();cartItem!=cart.end();cartItem++){
 			DB::updateItem((*cartItem).name,(*cartItem).amount);
+			pendingList[choice] = 0;
 		}
+		cart.clear();
 	}
 	
 	for(auto Item = list.begin();Item!=list.end();Item++){
 		if((*Item).name == choice){
 			int amount;
-			
 			std::cout << "Amount: ";
 			std::cin >> amount;
 			
+			pendingList[choice] += amount;
+			
 			system("cls");
-			if(amount > (*Item).amount){
+			if(pendingList[choice] > (*Item).amount){
 				system("cls");
-				std::cout << "\n[ERROR] Not Enought Items\n\n";
+				std::cout << "\n\n          [ERROR] Not Enought Items\n\n";
 				break;
 			}
 			
